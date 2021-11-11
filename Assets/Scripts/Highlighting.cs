98491bcbb180e4e3b93b01820325fcb161162d0b
jeniferirwin <jenifer.irwin@gmail.com>
2021-11-11 08:10:15 -0500

    Tiles now react to being moused over and clicked on

--- Assets/Scripts/Highlighting.cs
+++ Assets/Scripts/Highlighting.cs
@@ -6,18 +6,85 @@ using UnityEngine.InputSystem;
 
 public class Highlighting : MonoBehaviour
 {
+    public Tile[] tiles;
+    [SerializeField]
+    private Camera cam;
     [SerializeField]
     private Tilemap map;
     [SerializeField]
     private Grid grid;
-
+    
     private Vector2 mousePosition;
+    private Vector3Int prevCell;
+    private Vector3 worldPoint;
+    
+    public Color mouseOverColor;
+    public Color mouseDownColor;
+    private Color defaultColor = Color.white;
 
-    public void OnMouseover(InputAction.CallbackContext context)
+    private void Awake()
     {
-        mousePosition = (Vector3) context.ReadValue<Vector2>();
-        var tilePos = map.WorldToCell(mousePosition);
-        map.SetColor(tilePos, Color.blue);
+        mousePosition = Vector3.zero;
+        prevCell = Vector3Int.zero;
+        //StartCoroutine(ReportDebugInfo());
     }
 
+    public void OnMouseOver(InputAction.CallbackContext context)
+    {
+        mousePosition = (Vector3) context.ReadValue<Vector2>();
+        // highlight tile
+        var cell = CellFromMouse(mousePosition,map);
+        if (prevCell != cell)
+        {
+            Highlight(prevCell, defaultColor);
+            Highlight(cell, mouseOverColor);
+            prevCell = cell;
+        }
+    }
+    
+    private void Highlight(Vector3Int cell, Color color)
+    {
+        map.SetColor(cell, color);
+        map.RefreshTile(cell);
+    }
+
+    public void OnMouseDown(InputAction.CallbackContext context)
+    {
+        if (context.canceled) return;
+        var cell = CellFromMouse(mousePosition, map);
+        Highlight(cell, mouseDownColor);
+        map.RefreshTile(cell);
+    }
+    
+    public void OnMouseUp(InputAction.CallbackContext context)
+    {
+        if (!context.canceled) return;
+        var cell = CellFromMouse(mousePosition,map);
+        prevCell = cell;
+        if (!map.HasTile(cell))
+        {
+            var rand = Random.Range(0,4);
+            Debug.Log($"Setting new tile at {cell}: {tiles[rand].name}");
+            map.SetTile(cell, tiles[rand]);
+            map.RefreshTile(cell);
+        }
+        Highlight(cell, mouseOverColor);
+    }
+
+    private Vector3Int CellFromMouse(Vector2 pos, Tilemap tilemap)
+    {    
+        var worldPoint = cam.ScreenToWorldPoint(mousePosition);
+        worldPoint.z = tilemap.transform.position.z;
+        return tilemap.WorldToCell(worldPoint);
+    }    
+
+    private IEnumerator ReportDebugInfo()
+    {
+        for (;;)
+        {
+            Debug.Log(worldPoint);
+            yield return new WaitForSeconds(1);
+        }
+
+    }
 }
