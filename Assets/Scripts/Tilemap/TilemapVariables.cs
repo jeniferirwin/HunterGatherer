636b12a50f00eeb4b416b595aea16baf3a762c28
jeniferirwin <jenifer.irwin@gmail.com>
2021-11-13 04:05:28 -0500

    Cleaned up some experimental stuff

--- Assets/Scripts/Tilemap/TilemapVariables.cs
+++ Assets/Scripts/Tilemap/TilemapVariables.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEditor;
 
 [ExecuteInEditMode]
 public class TilemapVariables : MonoBehaviour
@@ -10,7 +11,8 @@ public class TilemapVariables : MonoBehaviour
     [SerializeField] private int width;
     [SerializeField] private int height;
     public Vector3Int[] cells;
-    
+    public CellData[] cellData;
+
     private void Start()
     {
         PopulateCellArray();
@@ -18,29 +20,42 @@ public class TilemapVariables : MonoBehaviour
 
     private void ShowCells()
     {
-        
+
     }
-    
+
     public void PopulateCellArray()
     {
-        cells = new Vector3Int[width * height];    
+        cells = new Vector3Int[width * height];
+        cellData = new CellData[width * height];
         int i = 0;
-        for (int y = 0; y < width; y++)
+        for (int y = 0; y > -width; y--)
         {
             for (int x = 0; x < height; x++)
             {
-                var pos = new Vector3Int(x,y,0);
+                var pos = new Vector3Int(x, y, 0);
                 cells[i] = pos;
+                cellData[i] = RandomizedCellData();
                 i++;
             }
         }
     }
-    
+
+    public CellData RandomizedCellData()
+    {
+        var data = new CellData(Random.Range(1, 11));
+        Debug.Log($"{data.Amount}");
+        return data;
+    }
+
     private void OnDrawGizmos()
     {
-        foreach (var cell in cells)
+        for (int i = 0; i < cells.Length; i++)
         {
-            Gizmos.DrawIcon(grid.CellToWorld(cell),"RegisteredCell",true);
+            var pos = grid.CellToWorld(cells[i]);
+            //Gizmos.DrawIcon(pos,"RegisteredCell",true);
+            //Handles.Label(pos, cellData[i].Amount.ToString());
+            //Handles.Label(pos, i.ToString());
         }
     }
+    
 }
